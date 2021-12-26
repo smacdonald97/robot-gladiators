69e30648a051f970692837cc9e67675bd5783645
smacdonald97 <smacdonald97@email.arizona.edu>
2021-12-25 20:32:08 -0700

    created inital javascript functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("What is your robot's name?")
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10+10);
+
+console.log("Our robot's name is Tony");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
