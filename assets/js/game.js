d4c497269273d33513e5ec2a886a0413d8228994
smacdonald97 <smacdonald97@email.arizona.edu>
2022-01-10 10:22:57 -0700

    created if else statements to gauge player health

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,44 @@
-var playerName = window.prompt("What is your robot's name?")
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10+10);
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-console.log("Our robot's name is Tony");
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+    //Log a resulting message to the console so we know that it worked.
+    playerName + "attacked" + enemyName + ". " + enemyName + "now has" + enemyHealth + "health remaining";
 
-//fight();
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    //Log a resulting message to the console so we know that it worked.
+    console.log(
+    enemyName + "attacked" + playerName + ". " + playerName + " now has" + playerHealth + "health remaining.");
+
+    if(enemyHealth <=0) {
+        window.alert(enemyName + " has died!");
+    }
+    else{
+        window.alert(enemyName + "still has" + enemyHealth + "health left.");
+    }
+
+    if(playerHealth <=0) {
+        window.alert(playerName + "has died!");
+    }
+
+    else {
+        window.alert(playerName + "still has" + playerHealth + "health left.");
+    }
+};
+
+fight();
