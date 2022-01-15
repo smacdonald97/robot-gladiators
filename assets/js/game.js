29496cd92e2df236643308d3ac5912134121b705
smacdonald97 <smacdonald97@email.arizona.edu>
2022-01-15 01:47:43 -0700

    added user high score to local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -195,6 +195,32 @@ var startGame =function() {
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
 
+    var highScore = localStorage.getItem("highscore");
+    if (highScore=== null) {
+        highScore = 0;
+    }
+
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + "now has the high score of " + playerInfo.money + "!");
+    }
+
+    else {
+        alert(playerInfo.name + "did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
+    var playAgainConfirm = window.confirm ("Would you like to play again?");
+
+    if(playAgainConfirm) {
+        startGame();
+    }
+
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+
     if (playerInfo.health > 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
     }else {
