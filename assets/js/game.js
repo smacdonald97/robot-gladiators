1b640760ee2a04399b914c7aba27a5158d6b8b04
smacdonald97 <smacdonald97@email.arizona.edu>
2022-01-15 00:52:21 -0700

    added random order to player and enemy turns

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,47 @@
+//I broke this code and can't figure it out. If you can, I'll buy you a beer.
 var randomNumber = function(min, max){
     var value= Math.floor(Math.random() * (max - min + 1) + min);
     return value;
 };
 
 var fight = function(enemy) {
+    var isPlayerTurn = true;
+        if (Math.random()> 0.5){
+            isPlayerTurn = false;
+        }
+        if (isPlayerTurn) {
+            if (fightOrSkip()){
+                break;
+            }
+            var damage = randomNumber(playerInfo.attack-3, playerInfo.attack);
+
+            enemy.health = Math.max(0, enemy.health - damage);
+            console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
+
+            if (enemy.health <=0) {
+                window.alert(enemy.name+ " has died!");
+
+                playerInfo.money= playerInfo.money +20;
+
+                break;
+            } else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+        } else {
+            var damage = randomNumber(enemy.attack -3, enemy.attack);
+
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
+            
+            if (playerInfo.health <=0) {
+                window.alert(playerInfo.name + " has died!");
+                break;
+            } else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
+        }
+
+        isPlayerTurn = !isPlayerTurn;
     while (playerInfo.health > 0 && enemy.health > 0) {
         if(fightOrSkip()){
             break;
